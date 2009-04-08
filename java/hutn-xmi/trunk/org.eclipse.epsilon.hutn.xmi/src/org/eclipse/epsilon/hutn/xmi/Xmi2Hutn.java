1d85291ff6794079eaec7ca7f81acb115ef5775d
Louis Rose <louismrose@gmail.com>
2009-04-08 11:47:37 +0000

    Switched to using STAX parser.
    git-svn-id: svn+ssh://linux.cs.york.ac.uk/usr/louis/subversion@101 4b1e1f99-2fa5-4378-8f9c-78b5fe92308c

--- java/hutn-xmi/trunk/org.eclipse.epsilon.hutn.xmi/src/org/eclipse/epsilon/hutn/xmi/Xmi2Hutn.java
+++ java/hutn-xmi/trunk/org.eclipse.epsilon.hutn.xmi/src/org/eclipse/epsilon/hutn/xmi/Xmi2Hutn.java
@@ -13,12 +13,12 @@
  */
 package org.eclipse.epsilon.hutn.xmi;
 
-import java.io.IOException;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLStreamException;
 
 import org.eclipse.epsilon.hutn.model.hutn.Spec;
 import org.eclipse.epsilon.hutn.unparser.HutnUnparser;
 import org.eclipse.epsilon.hutn.xmi.parser.XmiParser;
-import org.xml.sax.SAXException;
 
 public class Xmi2Hutn {
 
@@ -30,12 +30,12 @@ public class Xmi2Hutn {
 			spec     = new XmiParser(xmi).parse();
 			unparser = new HutnUnparser(spec);
 		
-		} catch (SAXException e) {
+		} catch (XMLStreamException e) {
 			throw new HutnXmiBridgeException(e);
 			
-		} catch (IOException e) {
+		} catch (FactoryConfigurationError e) {
 			throw new HutnXmiBridgeException(e);
-		}
+		} 
 	}
 	
 
--- java/hutn-xmi/trunk/org.eclipse.epsilon.hutn.xmi/src/org/eclipse/epsilon/hutn/xmi/parser/XmiParser.java
+++ java/hutn-xmi/trunk/org.eclipse.epsilon.hutn.xmi/src/org/eclipse/epsilon/hutn/xmi/parser/XmiParser.java
@@ -13,51 +13,113 @@
  */
 package org.eclipse.epsilon.hutn.xmi.parser;
 
-import java.io.IOException;
 import java.io.StringReader;
 
-import org.eclipse.epsilon.hutn.model.hutn.HutnFactory;
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.eclipse.epsilon.hutn.model.hutn.Spec;
-import org.eclipse.epsilon.hutn.xmi.parser.sax.ContentHandlerMultiplexer;
-import org.eclipse.epsilon.hutn.xmi.parser.sax.DebugContentHandler;
-import org.eclipse.epsilon.hutn.xmi.parser.sax.SpecGeneratingContentHandler;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.eclipse.epsilon.hutn.xmi.parser.generator.SpecGenerator;
+import org.eclipse.epsilon.hutn.xmi.util.HutnUtil;
 
 public class XmiParser {
 
 	private final String xmi;
 	
-	private XMLReader reader;
-	private SpecGeneratingContentHandler handler;
-	
+	private XMLStreamReader xmlStreamReader;
+	private final SpecGenerator generator = new SpecGenerator();
 	
 	public XmiParser(String xmi) {
 		this.xmi = xmi;
 	}
 
-	public Spec parse() throws SAXException, IOException {
-		initialiseXmlReader();
+	public Spec parse() throws XMLStreamException, FactoryConfigurationError {
+		xmlStreamReader = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(xmi));
 		
-		return doParse();
-	}
-	
-	private void initialiseXmlReader() throws SAXException {
-		reader  = XMLReaderFactory.createXMLReader();
-		handler = new SpecGeneratingContentHandler();
-		
-		reader.setContentHandler(new ContentHandlerMultiplexer(new DebugContentHandler(), handler));
-	}
-	
-	private Spec doParse() throws IOException {
-		try {
-			reader.parse(new InputSource(new StringReader(xmi)));
-			return handler.getSpec();
+		doParse();
 			
-		} catch (SAXException e) {
-			return HutnFactory.eINSTANCE.createSpec();
+		xmlStreamReader.close();
+		
+		
+		return generator.getSpec();
+	}
+	
+	private void doParse() throws XMLStreamException {
+		while (xmlStreamReader.hasNext()) {
+			final int token = xmlStreamReader.next();
+			
+			switch(token) {
+				case XMLStreamReader.START_ELEMENT:
+					processStartElement();
+					break;
+					
+				case XMLStreamReader.END_ELEMENT:
+					processEndElement();
+					break;
+					
+				default:
+					System.out.println(token);
+			}
+			
 		}
 	}
+
+	private boolean firstElement = true;
+	
+	private void processStartElement() {
+		final String tagName = xmlStreamReader.getLocalName();
+		
+		if (firstElement) {
+			processNamespaces();
+			generator.pushTopLevelClassObject(tagName);
+		
+			firstElement = false;
+			
+		} else {
+			final String xsiType = xmlStreamReader.getAttributeValue("http://www.w3.org/2001/XMLSchema-instance", "type");
+			
+			if (xsiType == null) {
+				final String type = HutnUtil.determineTypeOfFeatureFromMetaClass(generator.getCurrentClassObject(), tagName, "UnknownType");
+				generator.pushContainedClassObject(type, tagName);
+				
+			} else {
+    			generator.pushContainedClassObject(getLocalName(xsiType), tagName);
+			}
+		}
+		
+		processAttributes();
+	}
+	
+	private void processEndElement() {
+		generator.popCurrentClassObject();
+	}
+	
+	private void processNamespaces() {
+		for (int index = 0; index < xmlStreamReader.getNamespaceCount(); index++) {
+			if ("xmi".equals(xmlStreamReader.getNamespacePrefix(index)) ||
+				"xsi".equals(xmlStreamReader.getNamespacePrefix(index))) {
+					continue;
+			}
+			
+			generator.initialise(xmlStreamReader.getNamespaceURI(index));
+		}
+	}
+	
+	private void processAttributes() {
+		for (int index = 0; index < xmlStreamReader.getAttributeCount(); index++) {
+			if ("xmi".equals(xmlStreamReader.getAttributePrefix(index)) ||
+				"xsi".equals(xmlStreamReader.getAttributePrefix(index))) {
+					continue;
+			}
+			
+			generator.addSlot(xmlStreamReader.getAttributeLocalName(index), xmlStreamReader.getAttributeValue(index));
+		}
+	}
+	
+	
+	private static String getLocalName(String qualifiedName) {
+    	return qualifiedName.split(":")[1];
+    }
 }
