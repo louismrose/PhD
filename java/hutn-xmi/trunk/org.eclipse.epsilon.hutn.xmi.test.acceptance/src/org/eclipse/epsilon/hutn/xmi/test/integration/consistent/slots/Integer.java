92daf641bee26f191865b6b2c4e25e17957908f7
Louis Rose <louismrose@gmail.com>
2009-04-08 08:51:11 +0000

    Renamed "acceptance" tests to integration tests.
    git-svn-id: svn+ssh://linux.cs.york.ac.uk/usr/louis/subversion@92 4b1e1f99-2fa5-4378-8f9c-78b5fe92308c

---
+++ java/hutn-xmi/trunk/org.eclipse.epsilon.hutn.xmi.test.acceptance/src/org/eclipse/epsilon/hutn/xmi/test/integration/consistent/slots/Integer.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************
+ *
+ * $Id$
+ */
+package org.eclipse.epsilon.hutn.xmi.test.integration.consistent.slots;
+
+public class Integer extends AbstractSlotTest {
+
+	@Override
+	protected java.lang.String getXmi() {
+		return "numberOfChildren=\"2\"";
+	}
+
+	@Override
+	protected java.lang.String getExpectedFeatureName() {
+		return "numberOfChildren";
+	}
+	
+	@Override
+	protected Object getExpectedContent() {
+		return 2;
+	}
+}
