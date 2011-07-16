8d588e94d8af5dc2c1b6e2d058d43d6165aa83a7
Louis Rose <louismrose@gmail.com>
2011-07-16 13:07:51 +0100

    Removed YSC files.

--- YSC/imdb-scraper/imdb.php
+++
@@ -1,8 +0,0 @@
-<?
-	echo "Find a film:";
-	echo "<form method='post' action='findfilm.php'>";
-	echo "<input type='text' name='film'>";
-	echo "<br/><input type='submit' value='Submit'>";
-	echo "</form><br/>";
-
-?>
